fb8983b8681ad4c24aa64c3ee1b70e4497eddb50
Jasper Moelker <jasper@voorhoede.nl>
2024-02-04 11:53:04 +0100

    feat(theme): text block, purple theme, theme selector 🚧

---
+++ src/components/ThemeSelector/ThemeSelector.client.ts
@@ -0,0 +1,39 @@
+class ThemeSelector extends HTMLElement {
+  #selector: HTMLSelectElement;
+  #target: HTMLElement;
+  #theme?: string;
+  constructor() {
+    super();
+    this.#selector = this.querySelector('select') as HTMLSelectElement;
+    const targetSelector = this.dataset.target;
+    if (!targetSelector) {
+      throw new Error('ThemeSelector must have a `data-target` attribute');
+    }
+    this.#target = document.querySelector(targetSelector) as HTMLElement;
+    if (!this.#target) {
+      throw new Error(`No element found with selector: ${targetSelector}`);
+    }
+    const themeClass = [...this.#target.classList].find((className) =>
+      className.startsWith('theme--')
+    );
+    if (!themeClass) return;
+    const theme = themeClass.replace('theme--', '');
+    this.setTheme(theme);
+  }
+
+  connectedCallback() {
+    this.#selector.addEventListener('change', (event) => {
+      const theme = (event.target as HTMLSelectElement).value;
+      this.setTheme(theme);
+    });
+  }
+
+  setTheme(theme: string) {
+    this.#target.classList.remove(`theme--${this.#theme}`);
+    this.#target.classList.add(`theme--${theme}`);
+    this.#theme = theme;
+    this.#selector.value = theme;
+  }
+}
+
+customElements.define('theme-selector', ThemeSelector);
