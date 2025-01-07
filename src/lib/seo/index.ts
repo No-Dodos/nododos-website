ba777a0b23e4b777ea2abefbfc0052253d4c806d
Jasper Moelker <jasper@voorhoede.nl>
2025-01-07 18:13:37 +0100

    feature: (Dis)allow (AI) bots (#240)
    - Adds `allowAiBots` option to CMS (`app` model) to provide editors
    control if AI bots can access their content.
    - Adds `Disallow: /` for popular AI bots if option in CMS is set to
    false.
    - Adds `Disallow: /` for all agents if `noIndex` is selected in CMS SEO
    preferences and/or site is in preview mode.
    
    See new section in  `seo.md` for more details.
    
    Resolves #239
    
    1. Open preview link
    2. Navigate to the `/robots.txt` file
    3. Verify that it lists AI bots to disallow
    4. Verify that it allows other user agents
    5. Review the new setting in the CMS in the `control-bot-access`
    environment.
    6. Turn "Allow AI bots" on in the CMS
    7. Verify the disallow block is no longer in the `robots.txt` (need to
    test locally, as deploy preview serves static file generated during
    build)
    8. Turn "Prevent your site from being indexed by search engines?" on in
    the CMS (under SEO preferences)
    9. Verify the generic `User-agent: *` is now set to `Disallow: /` (need
    to test locally, as deploy preview serves static file generated during
    build)
    
    - [x] I have performed a self-review of my own code
    - [x] I have made sure that my PR is easy to review (not too big,
    includes comments)
    - [x] I have made updated relevant documentation files (in project
    README, docs/, etc)
    - ~I have added a decision log entry if the change affects the
    architecture or changes a significant technology~
    - [x] I have notified a reviewer
    
    <!-- Please strike through and check off all items that do not apply
    (rather than removing them) -->
    
    ---------
    
    Co-authored-by: Jurgen Beliën <info@jurgenbelien.nl>

---
+++ src/lib/seo/index.ts
@@ -0,0 +1,51 @@
+import type { Tag } from '@lib/types/datocms';
+import { getLocale } from '@lib/i18n';
+import type { SiteLocale } from '@lib/i18n.types';
+import { globalSeo } from '@lib/site.json';
+import aiRobotsTxt from './ai.robots.txt?raw';
+
+export type PageUrl = {
+  locale: SiteLocale,
+  pathname: string,
+};
+
+export const siteName = () => {
+  const locale = getLocale();
+  const localeSeo = globalSeo[locale as keyof typeof globalSeo];
+
+  return localeSeo.siteName;
+};
+
+export const titleSuffix = () => {
+  const locale = getLocale();
+  const localeSeo = globalSeo[locale as keyof typeof globalSeo];
+
+  return localeSeo.titleSuffix;
+};
+
+export const noIndexTag: Tag = {
+  attributes: { name: 'robots' },
+  content: 'noindex',
+  tag: 'meta',
+};
+
+export const titleTag = (title: string): Tag => ({
+  tag: 'title',
+  content: `${title} ${titleSuffix()}`,
+});
+
+export type RobotsTxtProps = {
+  allowAiBots: boolean,
+  allowAll: boolean,
+  siteUrl: string,
+}
+
+export const robotsTxt = ({ allowAiBots, allowAll, siteUrl }: RobotsTxtProps) => `
+${allowAiBots ? '' : aiRobotsTxt}
+
+User-agent: *
+${allowAll ? 'Allow: /' : 'Disallow: /'}
+
+Sitemap: ${siteUrl}/sitemap-index.xml
+
+`.trim();
